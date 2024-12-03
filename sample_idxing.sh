339620b4729e2cb4c3607bac4b5f6c8baee7cf15
Lucile <lucil_z@berkeley.edu>
2024-12-03 03:10:27 -0800

    Add comments to indexing code

--- sample_idxing.sh
+++ sample_idxing.sh
@@ -6,10 +6,15 @@ if [ "$#" -ne 1 ]; then
     exit 1
 fi
 
+#Extract sample name
 FILE_NAME=$1
 SAMPLE_NAME="${FILE_NAME%.gff3}"
 
+#sort gff file for tabix
 jbrowse sort-gff "${FILE_NAME}" > "${SAMPLE_NAME}_genes.gff"
+#delete any empty rows
 grep -v "^$" "${SAMPLE_NAME}_genes.gff" > "${SAMPLE_NAME}_clean_genes.gff"
+#compress file
 bgzip "${SAMPLE_NAME}_clean_genes.gff"
+#index compressed annotated genome
 tabix "${SAMPLE_NAME}_clean_genes.gff.gz"
