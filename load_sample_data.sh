b135eb8ea3d33d2c57094514daa39601aab331c3
anya-raju <90742646+anya-raju@users.noreply.github.com>
2024-12-09 00:41:45 -0800

    Remove HHV-8 extra strains

--- load_sample_data.sh
+++ load_sample_data.sh
@@ -6,8 +6,8 @@
 all_samples=("sample_data/HHV1_X14112" "sample_data/HHV2_Z86099" "sample_data/HHV3_X04370" 
              "sample_data/HHV4_NC_007605" "sample_data/HHV5_X17403" "sample_data/HHV6_X83413" 
              "sample_data/HHV6_AB021506" "sample_data/HHV6_AF157706" "sample_data/HHV7_U43400" 
-             "sample_data/HHV7_AF037218" "sample_data/HHV8_U75698" "sample_data/HHV8_U93872" 
-             "sample_data/HHV8_AF148805" "sample_data/McHV1_AF533768" "sample_data/MuGHV4_U97553")
+             "sample_data/HHV7_AF037218" "sample_data/HHV8_AF148805" "sample_data/McHV1_AF533768" 
+             "sample_data/MuGHV4_U97553")
 
 # Loop through all samples
 for SAMPLE_NAME in "${all_samples[@]}"; do
